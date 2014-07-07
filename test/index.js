f2d997cca1e3e800a54f44024b1b9db117a0d77b
Phillip Alexander <git@phillipalexander.io>
2014-07-07 00:00:26 -0700

    Include additional test cases

--- test/index.js
+++ test/index.js
@@ -6,4 +6,12 @@ describe('thirteen', function() {
     expect(thirteen).to.be.a("function");
     done();
   });
+  it('should return its argument multiplied by thirteen', function(done) {
+    expect(thirteen(10)).to.eql(130);
+    done();
+  });
+  it('should NOT return its argument multiplied by twelve', function(done) {
+    expect(thirteen(10)).to.not.eql(120);
+    done();
+  });
 });
