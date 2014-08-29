7fcf4caacad5ddf7014a35fcf9c5edd2930f89ca
zzmp <zmp@umich.edu>
2014-08-28 22:45:29 -0700

    (feat) Allow number-like objects.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 module.exports = function thirteen(number) {
   if (number !== undefined && typeof number !== "number") {
-    return new Error("I can only deal with numbers");
+    // Can number be cast to a number?
+    if (!isNaN(+number.toString())) {
+      return +number.toString() * 13;
+    } else if (!isNaN(+number.valueOf())) {
+      return +number.valueOf() * 13;
+    } else {
+      return new Error("I can only deal with numbers");
+    }
   } else {
     return number * 13;
   }
